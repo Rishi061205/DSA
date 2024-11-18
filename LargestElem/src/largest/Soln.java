df0e2d93b0916559dc0f8a266971b3affd07e83f
Rishi061205 <161162405+Rishi061205@users.noreply.github.com>
2024-11-18 18:46:22 +0530

    Add files via upload

--- LargestElem/src/largest/Soln.java
+++ LargestElem/src/largest/Soln.java
@@ -2,18 +2,17 @@ package largest;
 public class Soln {
 
 	public static void main(String[] args) {
-		int[] arr= {10,20,40,50,60};
+		int[] arr= {100,20,40,500,60};
 		int res = LargestElem(arr);
             System.out.println("The largest number found is: " + res);
 		}
 	static int LargestElem(int arr[]) {
 		int max=arr[0];
-		for(int i=1;i<arr.length;i++) {
-			if(arr[i]>max) {
-				max=arr[i];
+		for(int n:arr) {
+			if(n>max) {
+				max=n;
 			}
 		}
 		return max;
 	}
-
 }
